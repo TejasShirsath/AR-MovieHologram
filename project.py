0f565cb3c607666bb5f5622c5dd1084f074a5de5
TejasShirsath <tejasshirsath5@gmail.com>
2024-09-08 18:20:49 +0530

    Add overlay frame function

--- project.py
+++ project.py
@@ -1,13 +1,41 @@
 import cv2
 
-camera = cv2.VideoCapture(0)
+# camera variables 
+mobileCamera = "http://192.168.1.100:8080/video"
+laptopCamera = 0
+camera = laptopCamera
+
+# Load the movie and the camera feed
+movie = cv2.VideoCapture('demo.mp4')
+cap = cv2.VideoCapture(camera)
+
+# Resize the window
+cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('frame', (1000, 500))
+
+
+# Function to overlay movie frame
+def overlayFrame(frame, smallFrame, frameWidth, frameHeight, x, y):
+    smallFrame_resized = cv2.resize(smallFrame, (frameWidth, frameHeight))
+    movie_height, movie_width = smallFrame_resized.shape[:2]
+    x_offset = int(x * window_width / 100)
+    y_offset = int(y * window_height / 100)
+    frame[y_offset:y_offset+movie_height, x_offset:x_offset+movie_width] = smallFrame_resized
 
 while True:
-    ret, frame = camera.read()
-    cv2.imshow('output', frame)
+    success, frame = cap.read()
+    movie_success, movie_frame = movie.read()
 
-    if cv2.waitKey(1) == ord('q'):
+    window_height, window_width = frame.shape[:2]
+
+    if movie_success:
+        overlayFrame(frame, movie_frame, 200, 150, 50, 10)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-camera.release()
+    
+cap.release()
+movie.release()
 cv2.destroyAllWindows()
